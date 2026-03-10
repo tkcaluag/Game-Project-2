a510475ac98717d3e914441b52810be4ff6fa081
Tristan Caluag <97552369+tkcaluag@users.noreply.github.com>
2026-03-10 13:39:06 -0700

    Player Animations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,11 +9,13 @@ public class Movement : MonoBehaviour
     public float movementSpeed;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,17 @@ public class Movement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isRunning", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
